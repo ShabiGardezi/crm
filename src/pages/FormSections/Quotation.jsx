a00c5264afd49170eb9a068f2c11805c37cc78f5
ShabiGardezi <shabigardezi51214@gmail.com>
2023-10-05 14:24:56 -0500

    ticket forms

--- src/pages/FormSections/Quotation.jsx
+++ src/pages/FormSections/Quotation.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, Button } from "@material-ui/core";
+import axios from "axios"; // Import Axios
+
 import "../../styles/Forms/customforms.css";
+
 const QuotationComponent = () => {
   const [formData, setFormData] = useState({
     price: "",
@@ -15,31 +18,51 @@ const QuotationComponent = () => {
     });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    try {
+      const response = await axios.post("/api/your-endpoint", formData);
+      console.log("Data submitted successfully:", response.data);
+      // Clear the form after submission
+      setFormData({
+        price: "",
+        advanceprice: "",
+      });
+    } catch (error) {
+      // Handle errors (e.g., show an error message)
+      console.error("Error submitting data:", error);
+    }
+  };
+
   return (
     <div className="styleform">
-      <Grid container spacing={2}>
-        {/* <Grid item xs={12}>
-        <Typography variant="h6">Quotation</Typography>
-      </Grid> */}
-        <Grid item xs={6}>
-          <TextField
-            label="Price"
-            fullWidth
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-          />
+      <form onSubmit={handleSubmit}>
+        {/* Add onSubmit to the form element */}
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Price"
+              fullWidth
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Advance"
+              fullWidth
+              name="advanceprice"
+              value={formData.advanceprice}
+              onChange={handleChange}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label="Advance"
-            fullWidth
-            name="advanceprice"
-            value={formData.advanceprice}
-            onChange={handleChange}
-          />
-        </Grid>
-      </Grid>
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
--- src/pages/FormSections/TicketDetails.jsx
+++ src/pages/FormSections/TicketDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Grid, TextField, MenuItem } from "@material-ui/core";
+import { Grid, TextField, MenuItem, Button } from "@material-ui/core";
 import "../../styles/Forms/customforms.css";
-
+import axios from "axios";
 const TicketDetails = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const [formData, setFormData] = useState({
@@ -17,51 +17,71 @@ const TicketDetails = () => {
       [name]: value,
     });
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    try {
+      const response = await axios.post("/api/your-endpoint", formData);
+      console.log("Data submitted successfully:", response.data);
+      // Clear the form after submission
+      setFormData({
+        price: "",
+        advanceprice: "",
+      });
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+
   const formStyle = {
     whiteSpace: "pre-line",
   };
 
   return (
     <div className="styleform">
-      <Grid container spacing={2}>
-        {/* <Grid item xs={12}>
-          <Typography variant="h6">Ticket Details</Typography>
-        </Grid> */}
-        <Grid item xs={6}>
-          <TextField
-            label="Priority Level"
-            fullWidth
-            name="priorityLevel"
-            value={formData.priorityLevel}
-            onChange={handleChange}
-            select
-          >
-            <MenuItem value="Low">Low</MenuItem>
-            <MenuItem value="Moderate">Moderate</MenuItem>
-            <MenuItem value="High">High</MenuItem>
-          </TextField>
+      <form onSubmit={handleSubmit}>
+        {/* Add onSubmit to the form element */}
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Priority Level"
+              fullWidth
+              name="priorityLevel"
+              value={formData.priorityLevel}
+              onChange={handleChange}
+              select
+            >
+              <MenuItem value="Low">Low</MenuItem>
+              <MenuItem value="Moderate">Moderate</MenuItem>
+              <MenuItem value="High">High</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Assignor"
+              fullWidth
+              name="assignor"
+              value={formData.assignor}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6} style={formStyle}>
+            <TextField
+              label="Deadline"
+              fullWidth
+              name="dueDate"
+              value={formData.dueDate}
+              onChange={handleChange}
+              type="date"
+              defaultValue={new Date()}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label="Assignor"
-            fullWidth
-            name="assignor"
-            value={formData.assignor}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={6} style={formStyle}>
-          <TextField
-            label="Deadline"
-            fullWidth
-            name="dueDate"
-            value={formData.dueDate}
-            onChange={handleChange}
-            type="date"
-            defaultValue={new Date()}
-          />
-        </Grid>
-      </Grid>
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
