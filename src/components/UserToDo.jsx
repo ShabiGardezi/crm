69bc8afce4c2401a9deeaf5c5ec629e30ace0a70
ShabiGardezi <shabigardezi51214@gmail.com>
2023-10-30 14:08:04 -0500

    web seo form updated

--- src/components/UserToDo.jsx
+++ src/components/UserToDo.jsx
@@ -4,13 +4,16 @@ import AddToDo from "../pages/ToDoList/AddToDo";
 import ToDos from "../pages/ToDoList/ToDos";
 import ToDoContext from "../context/ToDoContext";
 import Header from "../pages/Header";
-import { Card, CardContent } from "@material-ui/core"; // Import Material-UI components
+import { Card, CardContent, Typography } from "@material-ui/core"; // Import Material-UI components
 import "../styles/ToDoList/ToDoList.css";
 
 const UserToDo = ({ wrapInCard, showHeader }) => {
   if (wrapInCard) {
     return (
       <>
+        <div className="ticketHeading">
+          <Typography variant="h5">Personal Notebook</Typography>
+        </div>
         {showHeader && <Header />} {/* Conditionally render the Header */}
         <div className="todolists">
           <ToDoContext>
