1321047c6841a12c1a6fb977b0d557371f20e8c9
Hamza <hkkh1441@gmail.com>
2023-11-13 13:33:42 -0800

    Styling changes made in SignUp and Login pages

--- src/pages/SIgnUp.jsx
+++ src/pages/SIgnUp.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  Container,
-  Grid,
-  Typography,
-  TextField,
-  Button,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
+    Container,
+    Grid,
+    Typography,
+    TextField,
+    Button,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
 } from "@material-ui/core";
 import toast from "react-hot-toast";
 import axios from "axios";
@@ -17,176 +17,181 @@ import "../styles/SignUp.css";
 import Header from "./Header";
 
 const SignUp = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    department: "",
-    role: "",
-  });
-  const [loading, setloading] = useState(false);
-  const [departments, setDepartments] = useState([]);
-  useEffect(() => {
-    const fetchDepartments = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/api/departments`
-        );
-        setDepartments(response.data.payload);
-      } catch (error) {
-        console.log(error);
-      }
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        department: "",
+        role: "",
+    });
+    const [loading, setloading] = useState(false);
+    const [departments, setDepartments] = useState([]);
+    useEffect(() => {
+        const fetchDepartments = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:5000/api/departments`
+                );
+                setDepartments(response.data.payload);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchDepartments();
+    }, []);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
     };
-    fetchDepartments();
-  }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Add your form submission logic here
+        try {
+            setloading(true);
+            const {username, email, password, role, confirmPassword, department} =
+                formData;
+            const userData = {
+                username,
+                email,
+                password,
+                role,
+                confirmPassword,
+                department,
+            };
+            const response = await axios.post(
+                "http://localhost:5000/api/user/signup",
+                {
+                    ...userData,
+                }
+            );
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-    try {
-      setloading(true);
-      const { username, email, password, role, confirmPassword, department } =
-        formData;
-      const userData = {
-        username,
-        email,
-        password,
-        role,
-        confirmPassword,
-        department,
-      };
-      const response = await axios.post(
-        "http://localhost:5000/api/user/signup",
-        {
-          ...userData,
+            console.log(response.data);
+            toast.success("Successfully added");
+        } catch (error) {
+            console.error(error.response.data.message);
+            toast.error(error.response.data.message);
         }
-      );
+        setloading(false);
+    };
 
-      console.log(response.data);
-      toast.success("Successfully added");
-    } catch (error) {
-      console.error(error.response.data.message);
-      toast.error(error.response.data.message);
-    }
-    setloading(false);
-  };
+    return (
+        <>
+            <Header/>
+            <Container maxWidth="lg">
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                        <div className="image-container">
+                            <img
+                                src={signupimg}
+                                alt="Left Img"
+                                style={{maxWidth: "90%", height: "auto"}}
+                            />
+                        </div>
+                    </Grid>
+                    <Grid className = "signup-form" item xs={12} sm={6}>
+                        <Typography variant="h4" align="center" gutterBottom style={{marginTop:"20px"}}>
+                            Sign Up
+                        </Typography>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                label="Username"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                variant="outlined"
+                                fullWidth
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                label="Email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                variant="outlined"
+                                fullWidth
+                                required
+                                margin="normal"
+                                style={{marginLeft:"15px"}}
 
-  return (
-    <>
-      <Header />
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <div className="image-container">
-              <img
-                src={signupimg}
-                alt="Left Img"
-                style={{ maxWidth: "90%", height: "auto" }}
-              />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="h4" align="center" gutterBottom>
-              Sign Up
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <TextField
-                label="Username"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-                required
-                margin="normal"
-              />
-              <TextField
-                label="Email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-                required
-                margin="normal"
-              />
-              <TextField
-                label="Password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-                type="password"
-                required
-                margin="normal"
-              />
-              <TextField
-                label="Confirm Password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                variant="outlined"
-                fullWidth
-                type="password"
-                required
-                margin="normal"
-              />
-              <FormControl variant="outlined" fullWidth margin="normal">
-                <InputLabel id="department-label">Department</InputLabel>
-                <Select
-                  labelId="department-label"
-                  id="department"
-                  name="department"
-                  value={formData.department}
-                  onChange={handleChange}
-                  label="Department"
-                >
-                  {departments.map((d) => (
-                    <MenuItem key={d._id} value={d._id}>
-                      {d.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+                            />
+                            <TextField
+                                label="Password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                variant="outlined"
+                                fullWidth
+                                type="password"
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                label="Confirm Password"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                variant="outlined"
+                                fullWidth
+                                type="password"
+                                required
+                                margin="normal"
+                                style={{marginLeft:"15px"}}
 
-              <FormControl variant="outlined" fullWidth margin="normal">
-                <InputLabel id="role-label">Role</InputLabel>
-                <Select
-                  labelId="role-label"
-                  id="role"
-                  name="role"
-                  value={formData.role}
-                  onChange={handleChange}
-                  label="Role"
-                >
-                  <MenuItem value="admin">admin</MenuItem>
-                  <MenuItem value="employee">employee</MenuItem>
-                </Select>
-              </FormControl>
+                            />
+                            <FormControl variant="outlined" fullWidth margin="normal">
+                                <InputLabel id="department-label">Department</InputLabel>
+                                <Select
+                                    labelId="department-label"
+                                    id="department"
+                                    name="department"
+                                    value={formData.department}
+                                    onChange={handleChange}
+                                    label="Department"
+                                >
+                                    {departments.map((d) => (
+                                        <MenuItem key={d._id} value={d._id}>
+                                            {d.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
 
-              <div className="btn-signup">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  fullWidth
-                >
-                  Sign Up
-                </Button>
-              </div>
-            </form>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
-  );
+                            <FormControl variant="outlined" fullWidth margin="normal" style={{marginLeft:"15px"}}
+                            >
+                                <InputLabel id="role-label">Role</InputLabel>
+                                <Select
+                                    labelId="role-label"
+                                    id="role"
+                                    name="role"
+                                    value={formData.role}
+                                    onChange={handleChange}
+                                    label="Role"
+
+                                >
+                                    <MenuItem value="admin">admin</MenuItem>
+                                    <MenuItem value="employee">employee</MenuItem>
+                                </Select>
+                            </FormControl>
+
+                            <div className="btn-signup">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    );
 };
 
 export default SignUp;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,146 +1,146 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import {
-  Grid,
-  Box,
-  Button,
-  FormControl,
-  TextField,
-  Typography,
-  Container,
-  Checkbox,
-  FormControlLabel,
+    Grid,
+    Box,
+    Button,
+    FormControl,
+    TextField,
+    Typography,
+    Container,
+    Checkbox,
+    FormControlLabel,
 } from "@material-ui/core";
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import {createTheme, ThemeProvider} from "@material-ui/core/styles";
 import SigninImg from "../assests/Rank-BPO-PVT-LTD-LOGO-02 copy.png";
 import "../styles/SignIn.css";
-import { Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
-import toast, { Toaster } from "react-hot-toast";
+import toast, {Toaster} from "react-hot-toast";
 
 const theme = createTheme();
 
 const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-  const navigate = useNavigate();
-  const url = process.env.REACT_APP_API_URL;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
+    const navigate = useNavigate();
+    const url = process.env.REACT_APP_API_URL;
 
-  // Load saved email and password from localStorage on component mount
-  useEffect(() => {
-    const savedEmail = localStorage.getItem("savedEmail");
-    const savedPassword = localStorage.getItem("savedPassword");
+    // Load saved email and password from localStorage on component mount
+    useEffect(() => {
+        const savedEmail = localStorage.getItem("savedEmail");
+        const savedPassword = localStorage.getItem("savedPassword");
 
-    if (savedEmail && savedPassword) {
-      setEmail(savedEmail);
-      setPassword(savedPassword);
-      setRememberMe(true);
-    }
-  }, []);
+        if (savedEmail && savedPassword) {
+            setEmail(savedEmail);
+            setPassword(savedPassword);
+            setRememberMe(true);
+        }
+    }, []);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-    try {
-      const response = await axios.post(
-        `http://localhost:5000/api/user/signin`,
-        { email, password }
-      );
-      localStorage.setItem("user", JSON.stringify(response.data.payload));
-      navigate("/home");
+        try {
+            const response = await axios.post(
+                `http://localhost:5000/api/user/signin`,
+                {email, password}
+            );
+            localStorage.setItem("user", JSON.stringify(response.data.payload));
+            navigate("/home");
 
-      toast.success("Login successful"); // Show success toast
-    } catch (error) {
-      console.log("Error logging in:", error);
-      toast.error("Login failed"); // Show error toast
-    }
-  };
+            toast.success("Login successful"); // Show success toast
+        } catch (error) {
+            console.log("Error logging in:", error);
+            toast.error("Login failed"); // Show error toast
+        }
+    };
 
-  React.useEffect(() => {
-    const rememberedEmail = localStorage.getItem("rememberedEmail");
-    const rememberedPassword = localStorage.getItem("rememberedPassword");
-    if (rememberedEmail && rememberedPassword) {
-      setEmail(rememberedEmail);
-      setPassword(rememberedPassword);
-      setRememberMe(true);
-    }
-  }, []);
+    React.useEffect(() => {
+        const rememberedEmail = localStorage.getItem("rememberedEmail");
+        const rememberedPassword = localStorage.getItem("rememberedPassword");
+        if (rememberedEmail && rememberedPassword) {
+            setEmail(rememberedEmail);
+            setPassword(rememberedPassword);
+            setRememberMe(true);
+        }
+    }, []);
 
-  return (
-    // <ThemeProvider theme={theme}>
-    <Container maxWidth="lg">
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <div className="image-container">
-            <img
-              src={SigninImg}
-              alt="Left Img"
-              style={{ maxWidth: "80%", height: "auto" }}
-            />
-          </div>
-        </Grid>
-        <Grid className="signin-form" item xs={12} md={6}>
-          <Typography variant="h4">Sign In</Typography>
+    return (
+        // <ThemeProvider theme={theme}>
+        <Container maxWidth="lg">
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    <div className="image-container">
+                        <img
+                            src={SigninImg}
+                            alt="Left Img"
+                            style={{maxWidth: "80%", height: "auto"}}
+                        />
+                    </div>
+                </Grid>
+                <Grid className="signin-form" item xs={12} md={6}>
+                    <Typography variant="h4" style={{marginTop:"15px"}}>Sign In</Typography>
 
-          <form onSubmit={handleLogin}>
-            <FormControl fullWidth margin="normal">
-              <TextField
-                type="email"
-                label="Email"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <TextField
-                type="password"
-                label="Password"
-                variant="outlined"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </FormControl>
-            <div>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={rememberMe}
-                    onChange={(e) => setRememberMe(e.target.checked)}
-                  />
-                }
-                label="Remember Me"
-              />
-            </div>
-            <Button
-              className="signin-btn"
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
-              Sign In
-            </Button>
-          </form>
-          <Grid item>
-            <Typography variant="body2">
-              Don't have an account?
-              <Link to="/signup">
-                <Button
-                  color="primary"
-                  style={{ textDecoration: "underline", cursor: "pointer" }}
-                >
-                  Sign Up
-                </Button>
-              </Link>
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
-    // </ThemeProvider>
-  );
+                    <form onSubmit={handleLogin}>
+                        <FormControl style={{marginTop: "10px"}} fullWidth>
+                            <TextField
+                                type="email"
+                                label="Email"
+                                variant="outlined"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </FormControl> <br/>
+                        <FormControl style={{marginTop: "10px"}} fullWidth>
+                            <TextField
+                                type="password"
+                                label="Password"
+                                variant="outlined"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </FormControl>
+                        <div style={{marginTop: "10px"}}>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={rememberMe}
+                                        onChange={(e) => setRememberMe(e.target.checked)}
+                                    />
+                                }
+                                label="Remember Me"
+                            />
+                        </div>
+                        <Button
+                            className="signin-btn"
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                    <Grid item>
+                        <Typography variant="body2" style={{marginTop: "10px"}}>
+                            Don't have an account?
+                            <Link to="/signup">
+                                <Button
+                                    color="primary"
+                                    style={{textDecoration: "underline", cursor: "pointer"}}
+                                >
+                                    Sign Up
+                                </Button>
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+        // </ThemeProvider>
+    );
 };
 
 export default SignIn;
