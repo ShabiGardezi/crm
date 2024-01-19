143f1fc0f3315efc2f895f66f818e129f4465eea
ShabiGardezi <shabigardezi51214@gmail.com>
2024-01-19 14:57:09 +0000

    closer commission sheet

--- src/pages/SalesSalarySheet/CloserSalarySheet.jsx
+++ src/pages/SalesSalarySheet/CloserSalarySheet.jsx
@@ -14,33 +14,8 @@ import Header from "../Header";
 export default function CloserSalarySheet() {
   const apiUrl = process.env.REACT_APP_API_URL;
   const [tickets, setTickets] = useState([]);
-  const [selectedCloser, setSelectedCloser] = useState("All");
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth()); // Default to current month
-  const [startDate, setStartDate] = useState(
-    new Date(new Date().getFullYear(), new Date().getMonth(), 1)
-  );
-  const [endDate, setEndDate] = useState(
-    new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
-  );
 
-  const handleStartDateSelect = (date) => {
-    setStartDate(date);
-  };
-
-  // Handle end date selection
-  const handleEndDateSelect = (date) => {
-    setEndDate(date);
-  };
-  const filteredTickets = tickets.filter((ticket) => {
-    const createdAtDate = new Date(ticket.createdAt);
-
-    // Check if the ticket's createdAt date is within the selected range
-    return (
-      (!startDate || createdAtDate >= startDate) &&
-      (!endDate ||
-        createdAtDate <= new Date(endDate.getTime() + 24 * 60 * 60 * 1000)) // Include end date
-    );
-  });
   useEffect(() => {
     const fetchTickets = async () => {
       try {
@@ -62,86 +37,10 @@ export default function CloserSalarySheet() {
     fetchTickets();
   }, []);
 
-  // Calculate total payment for each closer
-  const calculateTotalPayment = (closerName, startDate, endDate) => {
-    return tickets
-      .filter((ticket) => {
-        const createdAtDate = new Date(ticket.createdAt);
-
-        // Check if the ticket's createdAt date is within the selected range
-        return (
-          ticket.businessdetails.closer === closerName &&
-          (!startDate || createdAtDate >= startDate) &&
-          (!endDate ||
-            createdAtDate <= new Date(endDate.getTime() + 24 * 60 * 60 * 1000)) // Include end date
-        );
-      })
-      .reduce((total, ticket) => total + parseFloat(ticket.quotation.price), 0);
-  };
-
-  // Calculate commission based on total payment
-  const calculateCommission = () => {
-    const totalPayment = calculateTotalPayment(
-      selectedCloser,
-      startDate,
-      endDate
-    );
-
-    const commissionThresholdLow = 500;
-    const commissionThresholdMedium = 800;
-    const commissionThresholdHigh = 1500;
-    const commissionThresholdVeryHigh = 2000;
-
-    const ninetyPercent = 0.9 * totalPayment;
-
-    if (totalPayment <= commissionThresholdLow) {
-      return 0; // No commission
-    } else if (totalPayment <= commissionThresholdMedium) {
-      const commission = 0.025 * ninetyPercent;
-      return roundCommission(commission);
-    } else if (totalPayment <= commissionThresholdHigh) {
-      const commission = 0.05 * ninetyPercent;
-      return roundCommission(commission);
-    } else if (totalPayment <= commissionThresholdVeryHigh) {
-      const commission = 0.075 * ninetyPercent;
-      return roundCommission(commission);
-    } else {
-      const commission = 0.1 * ninetyPercent;
-      return roundCommission(commission);
-    }
-  };
-
-  const roundCommission = (commission) => {
-    // Round off to 2 decimal places with adjustment
-    const roundedCommission = Math.round(commission * 100) / 100;
-    return roundedCommission;
-  };
-
   // Get unique closer names
   const closers = [
     ...new Set(tickets.map((ticket) => ticket.businessdetails.closer)),
   ];
-
-  const calculateCommissionRate = (totalSalary) => {
-    const commissionThresholdLow = 500;
-    const commissionThresholdMedium = 800;
-    const commissionThresholdHigh = 1500;
-    const commissionThresholdVeryHigh = 2000;
-
-    const ninetyPercent = 0.9 * totalSalary;
-
-    if (totalSalary <= commissionThresholdLow) {
-      return 0; // No commission
-    } else if (totalSalary <= commissionThresholdMedium) {
-      return 2.5; // Commission rate for medium threshold
-    } else if (totalSalary <= commissionThresholdHigh) {
-      return 5; // Commission rate for high threshold
-    } else if (totalSalary <= commissionThresholdVeryHigh) {
-      return 7.5; // Commission rate for very high threshold
-    } else {
-      return 10; // Commission rate for exceeding very high threshold
-    }
-  };
 
   const calculateBountyAmount = (totalPayment) => {
     if (totalPayment >= 7000 && totalPayment <= 10000) {
@@ -153,67 +52,6 @@ export default function CloserSalarySheet() {
     }
   };
 
-  // Function to filter tickets for a specific month
-  const filterTicketsByMonth = (tickets, month) => {
-    return tickets.filter((ticket) => {
-      const createdAtDate = new Date(ticket.createdAt);
-      return createdAtDate.getMonth() === month;
-    });
-  };
-
-  // Function to calculate monthly values for each closer
-  const calculateMonthlyValues = async (closer, month) => {
-    try {
-      const response = await fetch(
-        `${apiUrl}/api/tickets/all-departments-ticket`
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch tickets");
-      }
-
-      const data = await response.json();
-      const monthlyTickets = filterTicketsByMonth(data.payload, month);
-
-      const totalSalary = calculateTotalPaymentForCloser(
-        closer,
-        monthlyTickets
-      );
-      const commissionRate = calculateCommissionRate(totalSalary);
-      const commissionableAmount = calculateCommissionableAmountForFronter(
-        closer,
-        monthlyTickets
-      );
-      const netSalary = totalSalary * 0.9 * (commissionRate / 100);
-      const bountyAmount = calculateBountyAmount(totalSalary);
-
-      return {
-        totalSalary,
-        commissionableAmount,
-        commissionRate,
-        netSalary,
-        bountyAmount,
-      };
-    } catch (error) {
-      console.error("Error fetching tickets:", error.message);
-      return null; // Handle error gracefully
-    }
-  };
-
-  // Function to calculate total payment for a closer with a specific set of tickets
-  const calculateTotalPaymentForCloser = (closer, tickets) => {
-    return tickets.reduce(
-      (total, ticket) => total + parseFloat(ticket.quotation.price),
-      0
-    );
-  };
-
-  // Function to calculate commissionable amount for a closer with a specific set of tickets
-  const calculateCommissionableAmountForFronter = (closer, tickets) => {
-    const totalPayment = calculateTotalPaymentForCloser(closer, tickets);
-    const commissionableAmount = 0.9 * totalPayment;
-    return roundCommission(commissionableAmount);
-  };
   const calculateSumOfTenPercent = (tickets, closer, selectedMonth) => {
     const closerTickets = tickets.filter(
       (ticket) =>
@@ -237,26 +75,40 @@ export default function CloserSalarySheet() {
     const createdAtDate = new Date(ticket.createdAt);
     return createdAtDate.getMonth() === selectedMonth;
   };
+  const calculateTotalForCloser = (closerName, selectedMonth) => {
+    // Point 1: Calculate total from quotation.price for the selected month
+    const totalFromQuotation = tickets
+      .filter((ticket) => {
+        const createdAtDate = new Date(ticket.createdAt);
+        return (
+          ticket.businessdetails.closer === closerName &&
+          createdAtDate.getMonth() === selectedMonth
+        );
+      })
+      .reduce((total, ticket) => total + parseFloat(ticket.quotation.price), 0);
 
-  // Function to calculate recurring sales
-  const calculateRecurringSalesForMonth = (closerPayments, selectedMonth) => {
-    // Filter payments for the selected month
-    const paymentsForMonth = closerPayments.filter((payment) => {
-      const paymentDate = new Date(payment.date);
-      return paymentDate.getMonth() === selectedMonth;
-    });
+    // Point 2: Check payment_history for each ticket for the selected month
+    const totalFromPaymentHistory = tickets
+      .map((ticket) => ticket.payment_history)
+      .filter((paymentHistory) => paymentHistory.length > 1) // Check if payment_history has more than one entry
+      .reduce((total, paymentHistory) => {
+        // Start from 1st index
+        for (let i = 1; i < paymentHistory.length; i++) {
+          const paymentDate = new Date(paymentHistory[i].date);
+          if (
+            paymentHistory[i].closer === closerName &&
+            paymentDate.getMonth() === selectedMonth
+          ) {
+            total += paymentHistory[i].payment;
+          }
+        }
+        return total;
+      }, 0);
 
-    // Filter payments with non-zero payment amounts and associated users
-    const validPayments = paymentsForMonth.filter(
-      (payment) => payment.payment > 0 && payment.user
-    );
+    // Calculate the overall total for the selected month
+    const overallTotal = totalFromQuotation + totalFromPaymentHistory;
 
-    // Take 90% of each valid payment and multiply by 5%
-    const recurringSales = validPayments
-      .map((payment) => 0.9 * payment.payment * 0.05)
-      .reduce((total, recurring) => total + recurring, 0);
-
-    return recurringSales;
+    return overallTotal;
   };
 
   return (
@@ -269,16 +121,6 @@ export default function CloserSalarySheet() {
           onChange={(e) => {
             const selectedDate = new Date(e.target.value);
             const selectedMonth = selectedDate.getMonth();
-            const values = closers.map((closer) =>
-              calculateMonthlyValues(closer, selectedMonth)
-            );
-            // Update state with selected start and end dates
-            setStartDate(
-              new Date(selectedDate.getFullYear(), selectedMonth, 1)
-            );
-            setEndDate(
-              new Date(selectedDate.getFullYear(), selectedMonth + 1, 0)
-            );
             setSelectedMonth(selectedMonth);
           }}
         />
@@ -289,7 +131,7 @@ export default function CloserSalarySheet() {
         style={{ textAlign: "center", marginBottom: "2%" }}
         fontFamily={"revert-layer"}
       >
-        Closer's Salary Sheet - {getMonthName(selectedMonth)}
+        Closer Commission Sheet - {getMonthName(selectedMonth)}
       </Typography>
 
       <TableContainer component={Paper}>
@@ -305,11 +147,11 @@ export default function CloserSalarySheet() {
           </TableHead>
           <TableBody>
             {closers.map((closer) => {
-              const totalSalary = calculateTotalPayment(
+              const totalSalary = calculateTotalForCloser(
                 closer,
-                startDate,
-                endDate
-              );
+                selectedMonth
+              ); // Calculate total for the closer
+
               const sumOfTenPercent = calculateSumOfTenPercent(
                 tickets,
                 closer,
@@ -318,76 +160,19 @@ export default function CloserSalarySheet() {
 
               const bountyAmount = calculateBountyAmount(totalSalary);
 
-              // Find the payment history for the current closer
-              const closerPaymentHistory = tickets
-                .filter((ticket) => ticket.businessdetails.closer === closer)
-                .map((ticket) => ticket.payment_history.slice(1)) // Skip the entry at index 0
-                .flat();
-
-              // Separate payments for the closer and other users
-              const closerPayments = [];
-              const otherUserPayments = [];
-
-              // Process each payment in the payment history
-              closerPaymentHistory.forEach((payment) => {
-                if (payment.user === closer) {
-                  // Payment associated with the closer
-                  closerPayments.push(payment);
-                } else {
-                  // Payment associated with another user
-                  otherUserPayments.push(payment);
-                }
-              });
-              const recurringSalesForMonth = calculateRecurringSalesForMonth(
-                closerPayments,
-                selectedMonth
-              );
               return (
-                <React.Fragment key={closer}>
-                  {/* Row for Closer's Payments */}
-                  <TableRow>
-                    <TableCell>{closer}</TableCell>
-                    <TableCell>
-                      <b>{`$${totalSalary}`}</b>
-                    </TableCell>
-                    <TableCell>
-                      <b>{`$${sumOfTenPercent.toFixed(2)}`}</b>
-                    </TableCell>
-                    <TableCell>
-                      <b>{`${bountyAmount.toFixed(2)} PKR`}</b>
-                    </TableCell>
-                    <TableCell>
-                      <b>{`$${calculateRecurringSalesForMonth(
-                        closerPayments,
-                        selectedMonth
-                      ).toFixed(2)}`}</b>
-                    </TableCell>
-                  </TableRow>
-
-                  {/* Rows for Other User's Payments */}
-                  {otherUserPayments.map((payment, index) => (
-                    <TableRow key={`${closer}-other-user-${index}`}>
-                      <TableCell>{payment.user}</TableCell>
-                      <TableCell colSpan={3}></TableCell>
-                      <TableCell>
-                        <b>{`$${calculateRecurringSalesForMonth(
-                          otherUserPayments,
-                          selectedMonth
-                        ).toFixed(2)}`}</b>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-
-                  {/* Row for Recurring Sales */}
-                  {/* <TableRow>
-                    <TableCell colSpan={4}></TableCell>
-                    <TableCell>
-                      <b>{`$${calculateRecurringSales(closerPayments).toFixed(
-                        2
-                      )}`}</b>
-                    </TableCell>
-                  </TableRow> */}
-                </React.Fragment>
+                <TableRow key={closer}>
+                  <TableCell>{closer}</TableCell>
+                  <TableCell>
+                    <b>{`$${totalSalary.toFixed(2)}`}</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>{`$${sumOfTenPercent.toFixed(2)}`}</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>{`${bountyAmount.toFixed(2)} PKR`}</b>
+                  </TableCell>
+                </TableRow>
               );
             })}
           </TableBody>
